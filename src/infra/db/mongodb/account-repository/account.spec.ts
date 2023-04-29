1673481e06e0144d09c9cfedd8e5bd710b286430
EmmanunelStocco <emmanuelrolimstocco@gmail.com>
2023-04-28 21:22:19 -0300

    test: ensure AccountMongoRepository returns an account on success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -1,4 +1,5 @@
 import { MongoHelper } from '../helpers/mongo-helper'
+import { AccountMongoRepository } from './account'
 
 describe('Account Mongo Repository', () => {
   beforeAll(async () => {
@@ -7,5 +8,15 @@ describe('Account Mongo Repository', () => {
 
   afterAll(async () => {
     await MongoHelper.disconnect()
+  })
+
+  test('Should return an account on success', async () => {
+    const sut = new AccountMongoRepository()
+    const isValid = await sut.add({
+      name: 'any_name',
+      email: 'any_email',
+      password: 'any_password'
+    })
+    expect(isValid).toBeTruthy()
   })
 })
