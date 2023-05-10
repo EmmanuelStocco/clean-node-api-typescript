a10bcec4553400b37f7a468b3694e1ffc409f9c3
EmmanunelStocco <emmanuelrolimstocco@gmail.com>
2023-05-09 22:12:56 -0300

    feat: ensure LogControllerDecorator calls controller with correct value

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -3,40 +3,15 @@ import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+import { type Controller } from '../../presentation/protocols'
+import { LogControllerDecorator } from '../decorators/log'
 
-export const makeSignUpController = (): SignUpController => {
+export const makeSignUpController = (): Controller => {
   const salt = 12
   const emailValidatorAdapter = new EmailValidatorAdapter()
   const bcryptAdapter = new BcryptAdapter(salt)
   const accountMongoRepository = new AccountMongoRepository()
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
-  return new SignUpController(emailValidatorAdapter, dbAddAccount)
+  const signUpController = new SignUpController(emailValidatorAdapter, dbAddAccount)
+  return new LogControllerDecorator(signUpController)
 }
-
-// import { SignUpController } from '../../presentation/controllers/signUp/signup'
-// import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
-// import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
-// import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
-// import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
-// import { type HttpRequest, type Controller, type HttpResponse } from '../../presentation/protocols'
-
-// class LogControllerDecorator implements Controller {
-//   private readonly controller: Controller
-
-//   constructor (controller: Controller) {
-//     this.controller = controller
-//   }
-
-//   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-
-//   }
-// }
-
-// export const makeSignUpController = (): SignUpController => {
-//   const salt = 12
-//   const emailValidatorAdapter = new EmailValidatorAdapter()
-//   const bcryptAdapter = new BcryptAdapter(salt)
-//   const accountMongoRepository = new AccountMongoRepository()
-//   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
-//   return new SignUpController(emailValidatorAdapter, dbAddAccount)
-// }
