cd1a9f85f9c32bd298aea501977be0757e3f8d1b
EmmanunelStocco <emmanuelrolimstocco@gmail.com>
2023-05-12 22:07:43 -0300

    feat: ensure DbAuthentication calls updateAcessTokenRepository with correct values

--- src/data/usecases/authentication/db-authentitcation.ts
+++ src/data/usecases/authentication/db-authentitcation.ts
@@ -2,16 +2,24 @@ import { type Authentication, type AuthenticationModel } from '../../../domain/u
 import { type HashComparer } from '../../protocols/criptography/hash-comparer'
 import { type TokenGenerator } from '../../protocols/criptography/token-generator'
 import { type LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
+import { type UpdateAcessTokenRepository } from '../../protocols/db/update-access-token-repository'
 
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
   private readonly hashComparer: HashComparer
   private readonly tokenGenerator: TokenGenerator
+  private readonly updateAcessTokenRepositoryStub: UpdateAcessTokenRepository
 
-  constructor (loadAccountByEmailRepository: LoadAccountByEmailRepository, hashComparer: HashComparer, tokenGenerator: TokenGenerator) {
+  constructor (
+    loadAccountByEmailRepository: LoadAccountByEmailRepository,
+    hashComparer: HashComparer,
+    tokenGenerator: TokenGenerator,
+    updateAcessTokenRepositoryStub: UpdateAcessTokenRepository
+  ) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository
     this.hashComparer = hashComparer
     this.tokenGenerator = tokenGenerator
+    this.updateAcessTokenRepositoryStub = updateAcessTokenRepositoryStub
   }
 
   async auth (authentication: AuthenticationModel): Promise<string> {
@@ -20,6 +28,7 @@ export class DbAuthentication implements Authentication {
       const isValid = await this.hashComparer.compare(authentication.password, account.password)
       if (isValid) {
         const accessToken = await this.tokenGenerator.generate(account.id)
+        await this.updateAcessTokenRepositoryStub.update(account.id, accessToken)
         return accessToken
       }
     }
