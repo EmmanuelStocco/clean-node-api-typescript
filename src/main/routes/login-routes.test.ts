ecd3877ef2af9414090e9d8138ba09c97b2f4d5a
EmmanunelStocco <emmanuelrolimstocco@gmail.com>
2023-05-17 01:02:48 -0300

    test: login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -49,5 +49,15 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+
+    test('Should return 401 on success', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'emmanuel@gmail.com',
+          password: '123'
+        })
+        .expect(401)
+    })
   })
 })
