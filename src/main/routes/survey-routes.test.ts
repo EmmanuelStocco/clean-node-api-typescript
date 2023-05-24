5a250ed287d9d2d063f6216880407030ca6623b9
EmmanunelStocco <emmanuelrolimstocco@gmail.com>
2023-05-24 20:38:04 -0300

    test: ensure add survey route returns 204 if valid access token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -2,9 +2,12 @@ import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import { type Collection } from 'mongodb'
+import { sign } from 'jsonwebtoken'
+import env from '../config/env'
 
 describe('Survey Routes', () => {
   let surveyCollection: Collection
+  let accountCollection: Collection
 
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL as string)
@@ -17,12 +20,45 @@ describe('Survey Routes', () => {
   beforeEach(async () => {
     surveyCollection = MongoHelper.getCollection('surveys')
     await surveyCollection.deleteMany({})
+    accountCollection = MongoHelper.getCollection('account')
+    await accountCollection.deleteMany({})
   })
 
   describe('POST /surveys', () => {
     test('Should return 403 on survey whitout accessToken', async () => {
       await request(app)
         .post('/api/surveys')
+        .send({
+          question: 'Question',
+          answers: [{
+            answer: 'Answer 1',
+            image: 'http://image-name.com'
+          }, {
+            answer: 'Answer 2'
+          }]
+        })
+        .expect(403)
+    })
+
+    test('Should return 204 on survey whit valid accessToken', async () => {
+      const res = await accountCollection.insertOne({
+        name: 'Emmanuel',
+        email: 'emmanuel@gmail.com',
+        password: '123',
+        role: 'admin'
+      })
+      const id = res.insertedId
+      const accessToken = sign({ id }, env.jwtSecret)
+      await accountCollection.updateOne({
+        _id: id
+      }, {
+        $set: {
+          accessToken
+        }
+      })
+      await request(app)
+        .post('/api/surveys')
+        .set('x-acces-token', accessToken)
         .send({
           question: 'Question',
           answers: [{
