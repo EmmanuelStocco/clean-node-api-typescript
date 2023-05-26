04101ce508299b87fe2180db9fc03b78b5d38e5d
EmmanunelStocco <emmanuelrolimstocco@gmail.com>
2023-05-26 17:01:55 -0300

    feat: ensure SurveyMongoRepository load all surveys

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,10 +1,19 @@
 import { type AddSurveyRepository } from '../../../../data/protocols/db/survey/add-survey-repository'
+import { type LoadSurveysRepository } from '../../../../data/protocols/db/survey/load-survey-repository'
+import { type SurveyModel } from '../../../../domain/models/survey'
 import { type AddSurveyModel } from '../../../../domain/usecases/add-survey'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class SurveyMongoRepository implements AddSurveyRepository {
+export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository {
   async add (surveyData: AddSurveyModel): Promise<void> {
     const surveyCollection = MongoHelper.getCollection('surveys')
     await surveyCollection.insertOne(surveyData)
+  }
+
+  async loadAll (): Promise<SurveyModel[]> {
+    const surveyCollection = await MongoHelper.getCollection('surveys')
+    const surveys: any[] = await surveyCollection.find().toArray()
+    console.log('surveys', surveys, typeof (surveys))
+    return surveys
   }
 }
